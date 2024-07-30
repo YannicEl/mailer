59a8c15385159d052f8027671dadbc98e95115d9
Yannic <yannic.ellhotka@gmail.com>
2024-07-30 08:16:30 +0000

    stuff

--- frontend/src/routes/api/+server.ts
+++ frontend/src/routes/api/+server.ts
@@ -2,8 +2,5 @@ import { text } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
 
 export const fallback: RequestHandler = async ({ request }) => {
-	const json = await request.json();
-	console.log(json);
-
 	return text('Hello Wrold!');
 };
