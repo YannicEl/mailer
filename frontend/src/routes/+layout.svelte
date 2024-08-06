b8980da3ce0080bb54ed90ccc68ba0b6b76af70b
Yannic <me@yannic.at>
2024-08-06 11:20:27 +0200

    env stuff

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -2,9 +2,6 @@
 	import '@unocss/reset/tailwind.css';
 	import 'virtual:uno.css';
 	import '../main.css';
-	import { PUBLIC_FRONTEND_URL } from '$env/static/public';
 </script>
-
-{PUBLIC_FRONTEND_URL}
 
 <slot />
