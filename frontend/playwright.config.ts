3e4a81fd5c2d1cc11964eb7d3fc0dc2f5e3b0018
Yannic <me@yannic.at>
2024-08-07 18:26:06 +0200

    added tests

---
+++ frontend/playwright.config.ts
@@ -0,0 +1,35 @@
+import { devices, type PlaywrightTestConfig } from '@playwright/test';
+
+const config: PlaywrightTestConfig = {
+	webServer: {
+		command: 'pnpm build && pnpm preview',
+		port: 4173,
+	},
+	testDir: 'tests',
+	testMatch: /(.+\.)?(test|spec)\.[jt]s/,
+
+	projects: [
+		{
+			name: 'Desktop Chrome',
+			use: devices['Desktop Chrome'],
+		},
+		{
+			name: 'Desktop Safari',
+			use: devices['Desktop Safari'],
+		},
+		{
+			name: 'Desktop Firefox',
+			use: devices['Desktop Firefox'],
+		},
+		{
+			name: 'Mobile Chrome',
+			use: devices['Pixel 5'],
+		},
+		{
+			name: 'Mobile Safari',
+			use: devices['iPhone SE'],
+		},
+	],
+};
+
+export default config;
